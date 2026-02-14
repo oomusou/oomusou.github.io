860c428c61951d74f81cd35d9242d79d733a7d88
oomusou <oomusou@gmail.com>
2026-02-14 08:52:15 +0800

    Site updated: 2026-02-14 08:52:01

---
+++ js/hanabi-browser.min.js
@@ -0,0 +1,7 @@
+/*!
+ * Hexo Theme Suka | hanabi-browser.js
+ * Author: SukkaW
+ * Link: https://github.com/SukkaW/hexo-theme-suka
+ * License: GPL-3.0
+ */
+setTimeout(function(){for(var e,n=document.querySelectorAll("pre code"),t=0;e=n[t++];){var o=e.textContent;e.innerHTML=hanabi&&hanabi(o)||o}});
